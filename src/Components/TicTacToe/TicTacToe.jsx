8dd14ce793abfe084931ab2c4cdfc9db7560934e
kbouns <bounseng.kevin@gmail.com>
2024-05-15 12:00:09 +0200

    Bouton reset marche

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -3,14 +3,11 @@ import './TicTacToe.css'; // Importation du fichier CSS
 import circle_icon from '../assets/circle.png'; // Importation de l'icône de cercle
 import cross_icon from '../assets/cross.png'; // Importation de l'icône de croix
 
-let data = ["","","","","","","","",""];
-
-
+let data = ["", "", "", "", "", "", "", "", ""];
 
 const TicTacToe = () => {
-
-  let [count,setCount] = useState(0);
-  let [lock,setLock] = useState(false);
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
   let titleRef = useRef(null);
 
   const toggle = (e, num) => {
@@ -18,90 +15,112 @@ const TicTacToe = () => {
       return 0;
     }
     if (count % 2 === 0) {
-      e.target.innerHTML = `<img src="${cross_icon}" alt="Cross">`; // Correction de "innerHTLM" à "innerHTML"
+      e.target.innerHTML = `<img src="${cross_icon}" alt="Cross">`;
       data[num] = "x";
-      setCount(count + 1); // Utilisation de l'incrémentation directe plutôt que ++count
+      setCount(count + 1);
     } else {
-      e.target.innerHTML = `<img src="${circle_icon}" alt="Circle">`; // Correction de "innerHTLM" à "innerHTML"
+      e.target.innerHTML = `<img src="${circle_icon}" alt="Circle">`;
       data[num] = "o";
-      setCount(count + 1); // Utilisation de l'incrémentation directe plutôt que ++count
+      setCount(count + 1);
     }
-    checkWin()
-  }
+    checkWin();
+  };
+
   const checkWin = () => {
-    if (data[0]===data[1] && data[1] === data[2] && data[2] !== "")
-      {
-        won(data[2]);
-      }
-      else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
-        {
-          won(data[5]);
-        }
-        else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
-          {
-            won(data[8]);
-          }
-          else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
-            {
-              won(data[6]);
-            }
-            else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
-              {
-                won(data[7]);
-              }
-              else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
-                {
-                  won(data[8]);
-                }
-                else if(data[0]===data[4] && data[4]===data[8] && data[8]!=="")
-                  {
-                    won(data[8]);
-                  }
-                  else if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
-                    {
-                      won(data[2]);
-                    }
-                    else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
-                      {
-                        won(data[6]);
-                      }
-  }
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    } else if (
+      data[3] === data[4] &&
+      data[4] === data[5] &&
+      data[5] !== ""
+    ) {
+      won(data[5]);
+    } else if (
+      data[6] === data[7] &&
+      data[7] === data[8] &&
+      data[8] !== ""
+    ) {
+      won(data[8]);
+    } else if (
+      data[0] === data[3] &&
+      data[3] === data[6] &&
+      data[6] !== ""
+    ) {
+      won(data[6]);
+    } else if (
+      data[1] === data[4] &&
+      data[4] === data[7] &&
+      data[7] !== ""
+    ) {
+      won(data[7]);
+    } else if (
+      data[2] === data[5] &&
+      data[5] === data[8] &&
+      data[8] !== ""
+    ) {
+      won(data[8]);
+    } else if (
+      data[0] === data[4] &&
+      data[4] === data[8] &&
+      data[8] !== ""
+    ) {
+      won(data[8]);
+    } else if (
+      data[0] === data[1] &&
+      data[1] === data[2] &&
+      data[2] !== ""
+    ) {
+      won(data[2]);
+    } else if (
+      data[2] === data[4] &&
+      data[4] === data[6] &&
+      data[6] !== ""
+    ) {
+      won(data[6]);
+    }
+  };
 
   const won = (winner) => {
     setLock(true);
-    if (winner==="x")
-      {
-        titleRef.current.innerHTML = `Bravo vous avez gagné : <img src="${cross_icon}"> à gagner`;
-      }
-      else
-      {
-        titleRef.current.innerHTML = `Bravo vous avez gagné : <img src="${circle_icon}"> à gagner`;
-      }
-      }
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Bravo vous avez gagné : <img src="${cross_icon}"> à gagner`;
+    } else {
+      titleRef.current.innerHTML = `Bravo vous avez gagné : <img src="${circle_icon}"> à gagner`;
+    }
+  };
 
-  
+  const reset = () => {
+    setLock(false);
+    setCount(0);
+    data = ["", "", "", "", "", "", "", "", ""];
+    titleRef.current.innerHTML = 'Morpion en <span>React</span>';
+    document.querySelectorAll('.boxes').forEach(box => box.innerHTML = '');
+  };
+
   return (
     <div>
       <div className="container">
-        <h1 className="title" ref={titleRef}>Morpion en <span> React</span></h1>
+        <h1 className="title" ref={titleRef}>
+          Morpion en <span> React</span>
+        </h1>
         <div className="board">
           <div className="row1">
-            <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 0) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 1) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 2) }}></div>
           </div>
           <div className="row2">
-            <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 3) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 4) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 5) }}></div>
           </div>
           <div className="row3">
-            <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
-            <div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 6) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 7) }}></div>
+            <div className="boxes" onClick={(e) => { toggle(e, 8) }}></div>
           </div>
         </div>
-        <button className="reset">Reset</button>
+        <button className="reset" onClick={reset}>Reset</button>
       </div>
     </div>
   );
